c7222af67c26970fe526ff7fe4815ee5ecfe2f11
Zane <zmccullar25@basecampcodingacademy.org>
2025-11-18 07:08:21 -0600

    computer aint cooperating

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('storefront/', storefront_page, name = "storefront"),
     path('register/', register_view, name = "register"),
     path('logout/', logout_view, name="logout")
+    path('item_detail/', item_view, name="details")
+
 ]
